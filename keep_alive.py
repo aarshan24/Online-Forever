e0febf88cd6218034b9f9200f67049901534d4f3
Aarshan24 <141824322+aarshan24@users.noreply.github.com>
2024-02-09 19:22:42 +0545

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,26 +1,15 @@
-from flask import Flask, request
+from flask import Flask
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def main():
-    global pending_status
-    # Handle incoming request from cronjobs
-    # Set pending_status to signal main.py to change status
-    pending_status = "change"
     return 'I am alive'  # Return a simple response
 
 def run():
-    try:
-        app.run(host="0.0.0.0", port=8080)
-    except OSError as e:
-        print(f"Error: {e}")
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    global pending_status
-    pending_status = ""  # Initialize pending_status
     server = Thread(target=run)
     server.start()
-
-pending_status = ""  # Initialize pending_status
