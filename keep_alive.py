ed94fa1d79d36c62469314f0e7628b3e72f49c57
Aarshan24 <141824322+aarshan24@users.noreply.github.com>
2024-02-09 14:26:50 +0545

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,23 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 from threading import Thread
-import time
-import requests
 
 app = Flask('')
 
-status = "online"  # Initial status
-
 @app.route('/')
 def main():
-    global status
-    # Check if the request is from the cronjobs
-    if request.headers.get('User-Agent') == 'cronjob-user-agent':
-        # Change status to offline
-        status = "offline"
-        # Sleep for 2 seconds
-        time.sleep(2)
-        # Change status to online
-        status = "online"
     return 'I am alive'  # Return a simple response
 
 def run():
