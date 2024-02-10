cefa9a49b49a17711b5cd1c658372fa48c2ba53f
Aarshan24 <141824322+aarshan24@users.noreply.github.com>
2024-02-10 13:41:53 +0545

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,8 @@ def on_open(ws):
                 time.sleep(59)
             except websocket.WebSocketConnectionClosedException:
                 print("WebSocket connection closed unexpectedly. Reconnecting...")
-                ws.connect()
+                ws = websocket.WebSocketApp(ws_url, on_open=on_open, on_message=on_message, on_error=on_error, on_close=on_close)
+                ws.run_forever()
 
     threading.Thread(target=update_status, daemon=True).start()
 
